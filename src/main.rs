a8a43c30b35b92554b10f109f61bae34ba276775
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-10 21:47:14 -0500

    feat: added decompression logic

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ use std::io::BufReader;
 use std::time::Instant;
 
 fn main() {
-    if args().len() != 3 {
-        eprintln!("Usage `source` `target`");
+    if args().len() != 4 {
+        eprintln!("Usage <source> <target>");
+        eprint!("--compress to compress source file to target file");
+        eprint!("--decompress to decompress source file to target file");
         return;
     }
 
+    // compress
     let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
     let output = File::create(args().nth(2).unwrap()).unwrap();
     let mut encoder = GzEncoder::new(output, Compression::default());
@@ -22,5 +25,13 @@ fn main() {
     let output = encoder.finish().unwrap();
     println!("Source len: {:?}", input.get_ref().metadata().unwrap().len());
     println!("Target len: {:?}", output.metadata().unwrap().len());
+    println!("Elapsed: {:?}", start.elapsed());
+
+    // decompress
+    let input = BufReader::new(File::open(args().nth(2).unwrap()).unwrap());
+    let mut output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut decoder = flate2::read::GzDecoder::new(input);
+    let start = Instant::now();
+    copy(&mut decoder, &mut output).unwrap();
     println!("Elapsed: {:?}", start.elapsed());
 }
